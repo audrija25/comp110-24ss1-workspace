ab57013a45c253fbf54a13f16c4ea8ebd7a87d50
audrija25 <abhatta@unc.edu>
2024-05-16 18:13:17 -0400

    "finished exercises 00"

---
+++ lessons/LS01-Objects-and-Data-Types.py
@@ -0,0 +1,30 @@
+"""Objects, Types, and Expressions Notes"""
+
+print("an example of numerical types:")
+
+print("an integer")
+print(237)
+print(type(237))
+
+print("a float")
+print(1.25)
+print(type(1.25))
+
+print("textual types")
+
+print("hello world")
+print(type("hello world"))
+
+print("booleans:")
+print(True)
+print(type(True))
+
+print("changing types")
+print(2.0)
+print(int(2.0))
+print(str(2.0))
+print(1_000 + 1_000_000)
+print(10 / 2)
+print("U" + "N" + "C")
+print("12345"[0])
+print("sppoky season"[2 + 2])
